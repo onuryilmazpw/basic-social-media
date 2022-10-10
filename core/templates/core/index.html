34aa349fb7f15c5d6e6e15d93522ba0a5ca3ec56
Onur Yılmaz <onuryilmazpw@gmail.com>
2022-10-11 00:54:32 +0300

    add homepage

---
+++ core/templates/core/index.html
@@ -0,0 +1,1 @@
+Burası Anasayfa
--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    template = "core/index.html"
+    context = {
+        "deneme" : "deneme"
+    }
+    return render(request, template, context)
