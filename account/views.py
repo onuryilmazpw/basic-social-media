e5f108941000b4a4b37326b77d16fd692ad26fff
Onur Yılmaz <onuryilmazpw@gmail.com>
2022-10-11 23:50:59 +0300

    add login system

--- account/views.py
+++ account/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def login_request(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+    
+    template = "account/login.html"
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            context = {
+                "error": "Kullanıcı adı veya parola yanlış"
+            }
+            return render(request, template, context)
+    else:
+        return render(request, template)
+
+def register_request(request):
+    pass
+
+def logout_request(request):
+    pass
--- basic_social_media/urls.py
+++ basic_social_media/urls.py
@@ -21,5 +21,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('core.urls')),
-    path("", include('userprofile.urls'))
+    path("", include('userprofile.urls')),
+    path("", include('account.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
