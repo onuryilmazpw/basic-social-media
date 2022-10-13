64b0902612ab9e0ef55e5051f8bf7e3bcf15103a
Onur Yılmaz <onuryilmazpw@gmail.com>
2022-10-13 14:37:46 +0300

    edit userprofile app views & templates

--- userprofile/views.py
+++ userprofile/views.py
@@ -11,7 +11,23 @@ def user_profile(request):
 
 def profile_edit(request):
     template = "userprofile/profile-edit.html"
+    user = request.user
     context = {
-        
+        "bilgi" : "Bilgilerde değişiklik yok!"
     }
+    if request.method == "POST":
+        if user.first_name != request.POST["firstname"]:
+            user.first_name = request.POST["firstname"]
+            context["bilgi"] = "Bilgiler güncellendi"
+    
+        if user.last_name != request.POST["lastname"]:
+            user.last_name = request.POST["lastname"]
+            context["bilgi"] = "Bilgiler güncellendi"
+        user.save()
+    else:
+        context["bilgi"] = "Değiştirmek istediğiniz bilgileri düzenleyip Kaydet butonuna basın."
+
+    context["firstname"] = user.first_name
+    context["lastname"] = user.last_name
+
     return render(request, template, context)
