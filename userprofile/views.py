cd671bce5480c159e6af55469512adf3017c94fc
Onur Yılmaz <onuryilmazpw@gmail.com>
2022-10-16 23:41:38 +0300

    edit profile-edit templates&views

--- userprofile/views.py
+++ userprofile/views.py
@@ -16,6 +16,14 @@ def profile_edit(request):
         "bilgi" : "Bilgilerde değişiklik yok!"
     }
     if request.method == "POST":
+        if user.username != request.POST["username"]:
+            user.username = request.POST["username"]
+            context["bilgi"] = "Bilgiler güncellendi"
+
+        if user.email != request.POST["email"]:
+            user.email = request.POST["email"]
+            context["bilgi"] = "Bilgiler güncellendi"
+        
         if user.first_name != request.POST["firstname"]:
             user.first_name = request.POST["firstname"]
             context["bilgi"] = "Bilgiler güncellendi"
@@ -23,10 +31,13 @@ def profile_edit(request):
         if user.last_name != request.POST["lastname"]:
             user.last_name = request.POST["lastname"]
             context["bilgi"] = "Bilgiler güncellendi"
+        
         user.save()
     else:
         context["bilgi"] = "Değiştirmek istediğiniz bilgileri düzenleyip Kaydet butonuna basın."
 
+    context["username"] = user.username
+    context["email"] = user.email
     context["firstname"] = user.first_name
     context["lastname"] = user.last_name
 
