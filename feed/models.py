8f5563e86b4f032705f8444723483729e37292a4
Onur Yılmaz <onuryilmazpw@gmail.com>
2022-10-17 13:30:33 +0300

    edit feed app (add new post feature)

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserPost(models.Model):
+    title = models.CharField(max_length=150)
+    post_text = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
--- feed/views.py
+++ feed/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from .models import UserPost
 
 # Create your views here.
 
 def add_post(request):
     template = "feed/add_post.html"
+    user = request.user
+
     context = {
-        
+        "bilgi" : ""
     }
+    if request.method == "POST":
+        title = request.POST["title"]
+        post_text = request.POST["post_text"]
+
+        UserPost.objects.create(title=title, post_text=post_text, owner=user)
+
+        context["post_text"] = post_text
+        context["title"] = title
+
+
     return render(request, template, context)
