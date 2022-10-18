22f83e065d8a395edde269b65bc2b4385a099478
Onur Yılmaz <onuryilmazpw@gmail.com>
2022-10-18 22:47:06 +0300

    edit feed models

--- feed/models.py
+++ feed/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
 class UserPost(models.Model):
     title = models.CharField(max_length=150)
     post_text = models.TextField()
+    post_date = models.DateTimeField(default=timezone.now)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
