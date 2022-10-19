9dc24cbd3883f173cd4c650a5901420af57ad682
Onur Yılmaz <onuryilmazpw@gmail.com>
2022-10-19 23:26:30 +0300

    add listing posts on homepage

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from feed.models import UserPost
 
 # Create your views here.
 
 def index(request):
     template = "core/index.html"
     context = {
-        
+        "all_post" : UserPost.objects.all()
     }
     return render(request, template, context)
