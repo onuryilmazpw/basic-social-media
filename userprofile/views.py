ef3b239745e04672036a50f0bbca22d88ecc1fc9
Onur Yılmaz <onuryilmazpw@gmail.com>
2022-11-01 23:33:24 +0300

    fix nonlogin profilepage

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from feed.models import UserPost
 
 # Create your views here.
 
 def user_profile(request):
     template = "userprofile/profile.html"
+    if not request.user.is_authenticated:
+        return redirect("home")
+
     context = {
         "all_post" : UserPost.objects.filter(owner=request.user).order_by('-post_date')
     }
