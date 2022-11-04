7d8909296244c423b12f6788c4f1afde6e17cbbe
Onur Yılmaz <onuryilmazpw@gmail.com>
2022-11-04 15:40:59 +0300

    add index viewes serach feature

--- core/views.py
+++ core/views.py
@@ -3,9 +3,18 @@ from feed.models import UserPost
 
 # Create your views here.
 
+
+#UserPost.objects.all().order_by('-post_date')
+#if request.GET['q'] and request.GET['q'] is not None:
+
 def index(request):
     template = "core/index.html"
+    try:
+        q = request.GET['q']
+        all_post = UserPost.objects.filter(title__contains=q).order_by('-post_date')
+    except:
+        all_post = UserPost.objects.all().order_by('-post_date')
     context = {
-        "all_post" : UserPost.objects.all().order_by('-post_date')
+        "all_post" : all_post
     }
     return render(request, template, context)
