a23cd8b35f6d64267023c023703abaefa2d795f1
Onur Yılmaz <onuryilmazpw@gmail.com>
2022-11-07 23:36:56 +0300

    edit filter queries

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from feed.models import UserPost
 
 # Create your views here.
@@ -8,7 +9,7 @@ def index(request):
 
     if 'q' in request.GET and request.GET.get('q'):
         q = request.GET['q']
-        all_post = UserPost.objects.filter(title__contains=q).order_by('-post_date')
+        all_post = UserPost.objects.filter(Q(title__contains=q) | Q(post_text__contains=q)).order_by('-post_date')
     else:
         all_post = UserPost.objects.all().order_by('-post_date')
         
