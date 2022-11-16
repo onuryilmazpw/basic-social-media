291faad1a56ff30bb3b2225a0e42d4c3c4398b37
Onur Yılmaz <onuryilmazpw@gmail.com>
2022-11-16 14:25:47 +0300

    add profile model

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    about = models.CharField(max_length=250)
+    account = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
